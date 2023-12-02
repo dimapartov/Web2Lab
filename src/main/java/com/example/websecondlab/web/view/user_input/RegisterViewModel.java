d9ca56ff6d6b2fd6181c683ad3a150b09f0c8d35
dimapartov <mr_kubarev@mail.ru>
2023-12-02 15:35:27 +0300

    Test

--- src/main/java/com/example/websecondlab/web/view/user_input/RegisterViewModel.java
+++ src/main/java/com/example/websecondlab/web/view/user_input/RegisterViewModel.java
@@ -4,7 +4,7 @@ import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-
+import com.example.websecondlab.util.validation.UniqueUsername;
 public class RegisterViewModel {
 
     @UniqueUsername
